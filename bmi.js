a4d1a77e2d7a24b5eacc8a10d66e3c060fa191de
Adjei Rapheal <ralphrapheals@gmail.com>
2024-02-17 02:09:48 +0000

    Fourth Commit

--- bmi.js
+++ bmi.js
@@ -3,12 +3,18 @@
 
 function bmi(weight, height){
     let bmiValue = weight / (height * height )
-    if (bmiValue < 18.5){
+    if (bmiValue >= 0 && bmiValue < 18.5){
         return "Your BMI value is "+ bmiValue + ". You are underweight"
     } else if (bmiValue >= 18.5 && bmiValue < 25){
-        return "Your BMI value is "+ bmiValue + ". You are underweight"
+        return "Your BMI value is "+ bmiValue + ". You have a normal weight"
+    } else if (bmiValue >= 25 && bmiValue < 30){
+        return "Your BMI value is "+ bmiValue + ". You are overweight"
+    } else if (bmiValue >= 30 && bmiValue < 40){
+        return "Your BMI value is "+ bmiValue + ". You have obesity"
+    }  else (bmiValue >= 40)
+        return "Your BMI value is "+ bmiValue + ". You have severe obesity"
     } 
-}
 
-const test = bmi(80, 2)
+
+const test = bmi(90, 1.2)
 console.log(test)
